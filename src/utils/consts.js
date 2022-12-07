88bc6acd424bebf13016fb20601c38d1ce56c24b
Andrey Mikulin <mikulin1999@gmail.com>
2022-12-07 18:23:48 +0500

    удалены лишние файлы и тп

--- src/utils/consts.js
+++
@@ -1,5 +0,0 @@
-export const BASE_URL = 'https://auth.nomoreparties.co'
-export const token = '54f3f87a-1850-4b2e-a915-ca3219f6554a';
-export const httpHeaders = {
-  'Content-Type': 'application/json'
-}
