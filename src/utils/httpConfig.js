fdaf0a2cc7a0b1fd494ce12a05eb664982de52e7
Andrey Mikulin <mikulin1999@gmail.com>
2022-12-07 18:25:06 +0500

    исправлена конфигурация axios

--- src/utils/httpConfig.js
+++ src/utils/httpConfig.js
@@ -1,11 +1,11 @@
 import axios from "axios";
 
 const $host = axios.create({
-  baseURL: process.env.REACT_APP_BASE_URL
+  baseURL: process.env.REACT_APP_BASE_URL | 'http://localhost:3000'
 })
 
 const $authHost = axios.create({
-  baseURL: process.env.REACT_APP_BASE_URL
+  baseURL: process.env.REACT_APP_BASE_URL | 'https://localhost:3000'
 })
 
 const authInterceptor = config => {
